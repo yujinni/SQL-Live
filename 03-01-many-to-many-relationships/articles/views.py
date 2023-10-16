ce26de9aa35a1e610e24e3ca94071217bb66c509
yujinni <yjoct13@gmail.com>
2023-10-16 16:46:20 +0900

    1016 live code

--- 03-01-many-to-many-relationships/articles/views.py
+++ 03-01-many-to-many-relationships/articles/views.py
@@ -4,7 +4,6 @@ from .models import Article, Comment
 from .forms import ArticleForm, CommentForm
 
 
-# Create your views here.
 def index(request):
     articles = Article.objects.all()
     context = {
